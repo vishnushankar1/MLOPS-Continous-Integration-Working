8b64e1f76647bffe76daa83648c5c3328587ad06
vishnushankar1 <vishnushankar123123@gmail.com>
2025-06-11 16:47:37 +0530

    checking code quality in the ci

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
+
 st.title('Power calculator')
 
-st.write("Enter a number to calulate its square,cube, and fifthe power")
+st.write("Enter a number to calculate its square, cube, and fifth power")
 
-n=st.number_input("enter a integer",step=1)
+n = st.number_input("Enter an integer", step=1)
 
-square=n**2
-cube=n**3
-fifth_power=n**5
+square = n ** 2
+cube = n ** 3
+fifth_power = n ** 5
 
-#display results
-st.write(f"the square of {n} is: {square}")
-st.write(f"the cube of { n} is: {cube}")
-st.write(f"the fifth power of {n} is: {fifth_power}")
+# Display results
+st.write(f"The square of {n} is: {square}")
+st.write(f"The cube of {n} is: {cube}")
+st.write(f"The fifth power of {n} is: {fifth_power}")
